b647f370d10ceb3fd34e552db04ac602ecfc3de6
10sr <8.slashes@gmail.com>
2018-12-17 19:40:34 +0900

    Add empty el

---
+++ with-venv.el
@@ -0,0 +1,30 @@
+;;; with-venv.el --- Execute with Python virtual environment enabled  -*- lexical-binding: t; -*-
+
+;; Author: 10sr <8.slashes [at] gmail [dot] com>
+;; URL: https://github.com/10sr/with-venv-el
+;; Version: 0.0.1
+;; Keywords: utility python venv
+
+;; This file is not part of GNU Emacs.
+
+;;   Licensed under the Apache License, Version 2.0 (the "License");
+;;   you may not use this file except in compliance with the License.
+;;   You may obtain a copy of the License at
+;;
+;;   http://www.apache.org/licenses/LICENSE-2.0
+;;
+;;   Unless required by applicable law or agreed to in writing, software
+;;   distributed under the License is distributed on an "AS IS" BASIS,
+;;   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;   See the License for the specific language governing permissions and
+;;   limitations under the License.
+
+;;; Commentary:
+
+;; Execute functions inside of Python vurtual environment.
+
+
+;;; Code:
+(provide 'with-venv)
+
+;;; with-venv.el ends here
